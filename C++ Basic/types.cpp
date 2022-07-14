cf06cb9bd1fd540a9dec0e25be3450c19a135e7e
Pranay Kumar Gorre <aranaykumar2611@gmail.com>
2022-07-14 21:49:23 +0530

    Data types

---
+++ C++ Basic/types.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int myNum = 5;               // Integer (whole number without decimals)
+    double myFloatNum = 5.99;    // Floating point number (with decimals)
+    char myLetter = 'D';         // Character
+    string myText = "Hello";     // String (text)
+    bool myBoolean = true;       // Boolean (true or false)
+    cout <<" Integer" <<myNum;
+    cout <<"\n Floating numbers"<<myFloatNum;
+    cout << "\nCharacter "<<myLetter;
+    cout << "\n String" <<myText;
+    cout <<"\n Boolean " <<myBoolean;
+    return 0;
+}
