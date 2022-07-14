39f3bdccf3468c5e07db3dc1cdd9c07fbdbbd9a8
Pranay Kumar Gorre <aranaykumar2611@gmail.com>
2022-07-14 14:44:44 +0530

    Output(cout)

---
+++ C++ Basic/print.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    cout << "this is print statement";
+    cout <<"\n this is new Line Statement";
+    cout << "You can also add endl at end to begin in new line "<< endl;
+    cout << " New line ";
+    
+
+    cout << " this is \t in the samew line ";
+    return 0;
+}
+// You can add as many cout objects as you want. However, note that it does not insert a new line at the end of the output:
+// Tip: Two \n characters after each other will create a blank line:
+// Another way to insert a new line, is with the endl manipulator:
+
+
+
+/*
+\t	Creates a horizontal tab	
+\\	Inserts a backslash character (\)	
+\"	Inserts a double quote character */
