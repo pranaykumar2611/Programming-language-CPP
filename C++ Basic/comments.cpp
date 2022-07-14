5862b15fbd9079971df3139a707db384e75a662e
Pranay Kumar Gorre <aranaykumar2611@gmail.com>
2022-07-14 15:14:31 +0530

    comments in C++

---
+++ C++ Basic/comments.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main (){
+    cout << "this Section is about commnets ";
+    cout << "Single Comments";
+    // Hello world
+    cout << "\n Multple comments ";
+    /* this 
+    is multy 
+    comment */ 
+}
