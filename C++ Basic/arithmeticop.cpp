b0689d27356522eb8cc6514ad281fc16098cb917
Pranay Kumar Gorre <aranaykumar2611@gmail.com>
2022-07-14 22:20:34 +0530

    Arithmetic Operations and Sum of Two numbers

---
+++ C++ Basic/arithmeticop.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int x=7,y=12;
+    cout << "Addtion "<<x+y <<endl;
+    cout << "Subtration " << x-y <<endl;
+    cout << "muliplication " <<x*y <<endl;
+    cout << "Divison " << x/y;
+    return 0;    
+}
---
+++ C++ Basic/sum.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int x=12,y=13;
+    int z = x+y;
+    cout << "Addtion of two number is: " << z;
+    return 0 ;
+}
